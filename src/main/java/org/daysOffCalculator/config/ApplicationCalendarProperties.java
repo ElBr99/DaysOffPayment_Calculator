6fd92ad4bda5396ac4325eb2dba3696b764aecde
ElBr99 <myemail@example.com>
2025-04-17 16:00:31 +0300

    Реализация калькулятора отпускных

--- src/main/java/org/daysOffCalculator/config/ApplicationCalendarProperties.java
+++ src/main/java/org/daysOffCalculator/config/ApplicationCalendarProperties.java
@@ -6,12 +6,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "application.calendar")
 @Value
 public class ApplicationCalendarProperties {
-
     String domain;
-
     String token;
-
     String country;
-
     String region;
 }
